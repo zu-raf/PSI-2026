bbe7991ff429b9f5d87eab54a2c702886f3def29
zu-raf <z.rafal@student.uw.edu.pl>
2026-03-05 19:17:41 +0100

    Update Zadanie_Text_mining_podstawy.R

--- Zajecia_03/Zadanie_Text_mining_podstawy.R
+++ Zajecia_03/Zadanie_Text_mining_podstawy.R
@@ -7,25 +7,17 @@ library(RColorBrewer)
 library(ggplot2)
 
 
-# Stworzenie funkcji do przetwarzania tekstu ----
 process_text <- function(file_path) {
-  # Wczytanie tekstu z pliku
   text <- tolower(readLines(file_path, encoding = "UTF-8"))
-  # Usunięcie znaków interpunkcyjnych i cyfr
   text <- removePunctuation(text)
   text <- removeNumbers(text)
-  # Usunięcie stop słów angielskich
   text <- removeWords(text, stopwords("en"))
-  # Podział tekstu na słowa
   words <- unlist(strsplit(text, "\\s+"))
-  # Usunięcie pustych elementów
   words <- words[words != ""]
   return(words)
   
 }
 
-
-# Stworzenie funkcji do obliczania częstości występowania słów ----
 word_frequency <- function(words) {
   freq <- table(words)
   freq_df <- data.frame(word = names(freq), freq = as.numeric(freq))
@@ -33,7 +25,7 @@ word_frequency <- function(words) {
   return(freq_df)
 }
 
-# Stworzenie funkcji do tworzenia chmury słów ----
+
 plot_wordcloud <- function(freq_df, title, color_palette = "Dark2") {
   wordcloud(words = freq_df$word, freq = freq_df$freq, min.freq = 16,
             colors = brewer.pal(8, color_palette))
@@ -41,10 +33,6 @@ plot_wordcloud <- function(freq_df, title, color_palette = "Dark2") {
 }
 
 
-# Przykładowe użycie funkcji ----
-
-
-# Dwa pliki txt równocześnie ----
 file_paths <- c("Biden2021.txt", "Biden2024.txt")
 custom_stopwords <- c("’m","’ve","’re","’s","’d","’ll","’t","’nt", "—", "–")
 
